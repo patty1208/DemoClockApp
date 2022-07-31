4f5f06e99e775144cfbef167c3e1f041deb98bfd
Patty <patty99491208@gmail.com>
2022-07-31 18:10:48 +0800

    chore: 【計時器】採用 Enum State 變數，新增 rawValue 新增 Enum State 型別且新增 rawValue，使計時器狀態更易懂

---
+++ DemoClockApp/Timer/Model/State.swift
@@ -0,0 +1,12 @@
+//
+//  State.swift
+//  DemoClockApp
+//
+//  Created by 林佩柔 on 2022/7/31.
+//
+
+import Foundation
+
+enum State: String {
+    case Start = "開始", Pause = "暫停", Resume = "繼續", Cancel = "取消"
+}
